414af277094f90f2164b61cf3567924dbc10c12a
bogdan <0966312126a@gmail.com>
2023-10-26 12:06:45 +0300

    update

--- js/main.js
+++ js/main.js
@@ -19,42 +19,6 @@ if (buttonToScrollFirstSection) {
   });
 }
 
-const stageSteps = document.querySelectorAll('#stage-of-cooperation .container .content .item .content-block');
-
-let margin = 0;
-stageSteps.forEach((item) => {
-  item.style.marginTop = `${margin}px`;
-  margin += 60;
-});
-
-const faqList = document.querySelectorAll('#faq .faq-list .faq-item');
-
-faqList.forEach((element) => {
-  element.addEventListener('click', (event) => {
-    const target = event.target;
-    const button = target.closest('.button-rounded');
-
-    if (button) {
-      const faqItem = target.closest('.faq-item');
-
-      faqList.forEach((faqItemElement) => {
-        if (faqItemElement !== element) {
-          faqItemElement.querySelector('.button-rounded').classList.remove('button-active');
-
-          faqItemElement.classList.remove('faq-item-active');
-        }
-      });
-      button.classList.toggle('button-active');
-
-      if (button.classList.contains('button-active')) {
-        faqItem.classList.add('faq-item-active');
-      } else {
-        faqItem.classList.remove('faq-item-active');
-      }
-    }
-  });
-});
-
 const selectForOrderTheService = document.querySelector('#select-for-order-the-service');
 const selectDropdown = selectForOrderTheService.querySelector('.dropdown-body');
 const dropdownItems = selectDropdown.querySelectorAll('.dropdown-item');
@@ -71,26 +35,6 @@ dropdownItems.forEach((component) => {
     selectForOrderTheService.classList.remove('select-open');
   });
 });
-
-if (window.innerWidth > 768) {
-  const chevrons = document.querySelectorAll('#work-with-us .chevrons img');
-
-  let prevChevrons = 0;
-  let currentChevrons = 0;
-
-  setInterval(() => {
-    const chevronsLength = chevrons.length;
-    prevChevrons = currentChevrons;
-    if (chevronsLength > currentChevrons + 1) {
-      currentChevrons += 1;
-    } else {
-      currentChevrons = 0;
-    }
-
-    chevrons[prevChevrons].src = 'image/workWithUs/ion_chevron-forward-outline(gray).svg';
-    chevrons[currentChevrons].src = 'image/workWithUs/ion_chevron-forward-outline(white).svg';
-  }, 200);
-}
 
 const openServices = document.querySelector('.open-header-services');
 const header = document.querySelector('header');
