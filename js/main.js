b393baba3bcece089878f4281a46621de2e9aaf7
bogdan <0966312126a@gmail.com>
2023-10-26 16:24:18 +0300

    update

--- js/main.js
+++ js/main.js
@@ -4,19 +4,19 @@ particlesJS.load('particles-js-duplicate', 'assets/particles.json');
 const buttonToBazar = document.querySelectorAll('.button-to-bazar');
 
 buttonToBazar.forEach((item) =>
-	item.addEventListener('click', () => {
-		window.open('https://www.bazar.club', '_blank');
-	}),
+  item.addEventListener('click', () => {
+    window.open('https://www.bazar.club', '_blank');
+  }),
 );
 
 const buttonToScrollFirstSection = document.querySelector('#button-scroll-to-first-section');
 
 if (buttonToScrollFirstSection) {
-	const firstSection = document.querySelector('section');
+  const firstSection = document.querySelector('section');
 
-	buttonToScrollFirstSection.addEventListener('click', () => {
-		firstSection.scrollIntoView({ behavior: 'smooth', block: 'center' });
-	});
+  buttonToScrollFirstSection.addEventListener('click', () => {
+    firstSection.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  });
 }
 
 const selectForOrderTheService = document.querySelector('#select-for-order-the-service');
@@ -24,16 +24,16 @@ const selectDropdown = selectForOrderTheService.querySelector('.dropdown-body');
 const dropdownItems = selectDropdown.querySelectorAll('.dropdown-item');
 
 selectForOrderTheService.addEventListener('click', (event) => {
-	event.stopPropagation();
-	selectForOrderTheService.classList.toggle('select-open');
+  event.stopPropagation();
+  selectForOrderTheService.classList.toggle('select-open');
 });
 selectDropdown.addEventListener('click', (event) => {
-	event.stopPropagation();
+  event.stopPropagation();
 });
 dropdownItems.forEach((component) => {
-	component.addEventListener('click', () => {
-		selectForOrderTheService.classList.remove('select-open');
-	});
+  component.addEventListener('click', () => {
+    selectForOrderTheService.classList.remove('select-open');
+  });
 });
 
 const openServices = document.querySelector('.open-header-services');
@@ -41,67 +41,66 @@ const header = document.querySelector('header');
 const burgerBtn = document.querySelector('.burger-btn');
 
 const closeBurgerBtn = () => {
-	header.classList.remove('open-burger');
-	document.body.style = undefined;
+  header.classList.remove('open-burger');
+  document.body.style = undefined;
 };
 const openBurgerBtn = () => {
-	header.classList.add('open-burger');
-	document.body.style = 'overflow: hidden;';
+  header.classList.add('open-burger');
+  document.body.style = 'overflow: hidden;';
 };
 
 openServices.addEventListener('click', () => {
-	if (header.attributes['open-services']) {
-		header.attributes.removeNamedItem('open-services');
-	} else {
-		header.setAttribute('open-services', '');
-	}
+  if (header.attributes['open-services']) {
+    header.attributes.removeNamedItem('open-services');
+  } else {
+    header.setAttribute('open-services', '');
+  }
 });
 burgerBtn.addEventListener('click', () => {
-	if (header.classList.contains('open-burger')) {
-		closeBurgerBtn();
-	} else {
-		openBurgerBtn();
-	}
+  if (header.classList.contains('open-burger')) {
+    closeBurgerBtn();
+  } else {
+    openBurgerBtn();
+  }
 });
 
 const buttonToForm = document.querySelectorAll('.button-to-form');
 const form = document.querySelector('#order-the-service .modal');
 
 buttonToForm.forEach((element) => {
-	element.addEventListener('click', () => {
-		closeBurgerBtn();
-		form.scrollIntoView({ behavior: 'smooth', block: 'center' });
-	});
+  element.addEventListener('click', () => {
+    closeBurgerBtn();
+    form.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  });
 });
 
 const menuDropdownList = document.querySelectorAll('.menu-dropdown');
 
 menuDropdownList.forEach((menuDropdown) => {
-	menuDropdown.addEventListener('click', (event) => {
-		const target = event.target;
-		const menu = target.closest('.menu-dropdown');
-		if (target.closest('.dropdown-title')) {
-			menu.classList.toggle('open');
-		}
-	});
+  menuDropdown.addEventListener('click', (event) => {
+    const target = event.target;
+    const menu = target.closest('.menu-dropdown');
+    if (target.closest('.dropdown-title')) {
+      menu.classList.toggle('open');
+    }
+  });
 });
 
 const menuChildDropdownList = document.querySelectorAll('.menu-child-dropdown');
 
 menuChildDropdownList.forEach((menuChildDropdown) => {
-	menuChildDropdown.addEventListener('click', (event) => {
-		const target = event.target;
-		const menu = target.closest('.menu-child-dropdown');
+  menuChildDropdown.addEventListener('click', (event) => {
+    const target = event.target;
+    const menu = target.closest('.menu-child-dropdown');
 
-		menuChildDropdownList.forEach((element) => {
-			console.log('sdfsdf');
-			if (element !== menuChildDropdown) {
-				element.classList.remove('open');
-			}
-		});
+    menuChildDropdownList.forEach((element) => {
+      if (element !== menuChildDropdown) {
+        element.classList.remove('open');
+      }
+    });
 
-		if (target.closest('.menu-child-title')) {
-			menu.classList.toggle('open');
-		}
-	});
+    if (target.closest('.menu-child-title')) {
+      menu.classList.toggle('open');
+    }
+  });
 });
--- js/swiper.js
+++ js/swiper.js
@@ -4,17 +4,17 @@ const mobileWidth = 431;
 const tabletWidth = 769;
 
 const swiperReviews = new Swiper('#swiper-reviews', {
-	slidesPerView: 1,
-	loop: true,
-	spaceBetween: 40,
-	breakpoints: {
-		[mobileWidth]: {
-			slidesPerView: 2,
-		},
-		[laptopLWidth]: {
-			slidesPerView: 4,
-		},
-	},
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 40,
+  breakpoints: {
+    [mobileWidth]: {
+      slidesPerView: 2,
+    },
+    [laptopLWidth]: {
+      slidesPerView: 4,
+    },
+  },
 });
 
 const reviewsButtonNext = document.querySelector('.reviews-button-next');
