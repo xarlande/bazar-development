378ef1b580c038a622d16818f848fed29245b849
bogdan <0966312126a@gmail.com>
2023-11-06 12:12:58 +0200

    update

--- js/swiper.js
+++ js/swiper.js
@@ -157,3 +157,83 @@ const ourProjectItems = {
     },
   ],
 };
+
+const ourProjectButtons = document.querySelectorAll('#our-projects .buttons-block button');
+const ourProjectSwiperBody = document.querySelector('#swiper-our-project .swiper-wrapper');
+
+const ourProjectsSwiper = new Swiper('#swiper-our-project', {
+  slidesPerView: 1,
+  loop: true,
+  breakpoints: {
+    [tabletWidth]: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+document.querySelector('.our-project-button-next').addEventListener('click', () => {
+  if (ourProjectSwiperBody.querySelectorAll('.swiper-slide').length) {
+    ourProjectsSwiper.slideNext();
+  }
+});
+
+document.querySelector('.our-project-button-prev').addEventListener('click', () => {
+  if (ourProjectSwiperBody.querySelectorAll('.swiper-slide').length) {
+    ourProjectsSwiper.slidePrev();
+  }
+});
+
+let currentOurProjectType = 'button2';
+
+const generateItemsForOurProjectSwiper = () => {
+  const container = document.createElement('div');
+
+  ourProjectItems[currentOurProjectType].forEach(item => {
+    let buttons = '';
+    item.tags.forEach(value => {
+      buttons += `<button class="button">${value}</button>`;
+    });
+    // prettier-ignore
+    const content = `<div class="swiper-slide">
+              <div class="project-block">
+                <img src="${item.image}" alt="img" />
+                <div class="our-project-block">
+                  <p class="title">${item.title}</p>
+                  <div class="link-block">
+                    ${buttons}
+                  </div>
+                  <div class="description-block">
+                    <p class="description">
+                    ${item.description}
+                    </p>
+                    <div class="button-block">
+                      <button class="button button-rounded" onclick="(${item.actionForButton})()"></button>
+                    </div>
+                  </div>
+                </div>
+                <div class="shadow"></div>
+              </div>
+            </div>`;
+
+    container.innerHTML += content;
+  });
+  ourProjectSwiperBody.innerHTML = container.innerHTML;
+
+  ourProjectsSwiper.update();
+  ourProjectsSwiper.slideTo(0);
+};
+
+ourProjectButtons.forEach(element => {
+  if (element.getAttribute('value') === 'button2') {
+    element.classList.add('button-active');
+  }
+
+  element.addEventListener('click', event => {
+    currentOurProjectType = event.target.getAttribute('value');
+    ourProjectButtons.forEach(item => item.classList.remove('button-active'));
+    event.target.classList.add('button-active');
+    generateItemsForOurProjectSwiper();
+  });
+});
+
+generateItemsForOurProjectSwiper();
